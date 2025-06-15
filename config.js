50c4384add2b0e20b0105c49bbb773da7640383c
quantumtaskai <thecyberlearn@gmail.com>
2025-06-15 22:28:57 +0530

    first commit
    supabase

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+const APP_CONFIG = {
+    supabaseUrl: 'https://ymjnjwtigyiajxgyikhb.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltam5qd3RpZ3lpYWp4Z3lpa2hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MTk0MzMsImV4cCI6MjA2NTE5NTQzM30.r7oESwxVE781baW4INZI_Ej_6hci21VTfyNcDZj_d3o',
+    n8nWebhookUrl: 'https://thecyberlearn.app.n8n.cloud/webhook/parse-query',
+    useN8n: true
+  };
