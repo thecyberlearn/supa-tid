696670128dd30b8988d266c4690596818a018160
amitrana01 <amitrana01@gmail.com>
2025-06-17 21:43:00 +0530

    fixing url

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 const APP_CONFIG = {
   supabaseUrl: 'https://ymjnjwtigyiajxgyikhb.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inltam5qd3RpZ3lpYWp4Z3lpa2hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MTk0MzMsImV4cCI6MjA2NTE5NTQzM30.r7oESwxVE781baW4INZI_Ej_6hci21VTfyNcDZj_d3o',
-  webhookUrl: 'https://m8taq6tk.rpcld.cc/webhook/trade',
+  webhookUrl: 'https://m8taq6tk.rpcld.cc/webhook/trading',
   useWebhook: true
 };
